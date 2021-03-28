7326076a1fea136e09e97d821336ecaf0ff6e180
799768 <799768@SAIT225783IT.ACDM.DS.SAIT.CA>
2021-03-28 14:37:00 -0600

    Added AdminFilter to filters

---
+++ src/java/filters/AdminFilter.java
@@ -0,0 +1,54 @@
+package filters;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import models.User;
+
+/**
+ *
+ * @author 799768
+ */
+public class AdminFilter implements Filter {
+
+    public AdminFilter() {
+    }    
+
+    public void doFilter(ServletRequest request, ServletResponse response,
+            FilterChain chain)
+            throws IOException, ServletException {
+        
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpSession session = httpRequest.getSession();
+        User user = (User) session.getAttribute("user");
+        
+        if (user == null) {
+            user = new User();
+        }
+        
+        if (user.getRole().getRoleId() != 1) {
+            HttpServletResponse httpResponse = (HttpServletResponse) response;
+            httpResponse.sendRedirect("notes");
+            return;
+        }
+
+        chain.doFilter(request, response);
+
+    }
+
+    public void destroy() {        
+    }
+
+    public void init(FilterConfig filterConfig) {
+    }
+}
